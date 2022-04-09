4931dfc566333023a5dbf8f1046c51ecf908669d
Daniar Suprianto <daniar@funda.nl>
2022-04-09 22:33:15 +0200

    feat: add image gallery

---
+++ components/ListingGallery.vue
@@ -0,0 +1,40 @@
+<!-- Could have used library such as Swiper, but that's for improvement -->
+<template>
+  <div>
+    <div class="p-2 border-b border-gray-200 pb-2">
+      <div class="relative overflow-hidden pb-full">
+        <img
+          :src="images[activeIndex].node.small"
+          :alt="`Should have very descriptive name ${activeIndex + 1}`"
+          class="absolute h-full w-full object-cover"
+        />
+      </div>
+    </div>
+
+    <ul
+      v-if="images.length > 1"
+      class="flex overflow-auto gap-2 py-2 px-4 border-b border-gray-200"
+    >
+      <li
+        v-for="(image, index) in images"
+        :key="image.node.id"
+        class="w-20 flex-shrink-0 border border-gray-200 rounded p-1"
+        :class="{ 'border-gray-400': activeIndex === index }"
+        @click="activeIndex = index"
+      >
+        <img :src="image.node.modal" class="h-full w-full object-cover" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { images: { type: Array, default: () => [] } },
+  data() {
+    return {
+      activeIndex: 0,
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,46 @@
 <template>
-  <Tutorial />
+  <div v-if="!$fetchState.pending" class="container pb-12">
+    <div class="md:flex">
+      <ListingGallery
+        v-if="listing.images.edges.length"
+        :images="listing.images.edges"
+        class="mb-4"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      store: {},
+      listing: {},
+      favorite: false,
+    }
+  },
+  fetch() {
+    this.fetchObjectData()
+  },
+  methods: {
+    async fetchObjectData() {
+      try {
+        const response = await fetch(
+          'https://gist.githubusercontent.com/Vugario/ca7cc468c344494ccae22b2c835e0c9f/raw/fe4669ee0989e78be1ed9ccd78ba178c695c305a/data.json'
+        )
+        const jsonData = await response.json()
+
+        this.store = {
+          name: jsonData.data.storefrontBySlug.name,
+          slug: jsonData.data.storefrontBySlug.slug,
+        }
+
+        this.listing = jsonData.data.storefrontBySlug.listing
+      } catch (error) {
+        console.error(error)
+      }
+    },
+  },
 }
 </script>
